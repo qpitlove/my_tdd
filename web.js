145ca1335f43d82396a5c472d89d6b96c795f830
qpitlove <qpitlove@gmail.com>
2012-06-04 02:49:06 +0000

    change port number 3000 -> 5000

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.get("/", function(request, response){
     response.send("Hello qpitlove");
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, function(){
     console.log("Listening on " + port);
 });
